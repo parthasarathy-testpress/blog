0a77b6eebfde3876fc762a6a583f6bb8d5b57da0
parthasarathy-testpress <parthasarathy@testpress.in>
2025-09-01 18:03:53 +0530

    feat: Add blog app

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post/list.html'
+    context_object_name = 'posts'
+    paginate_by = 3
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post/detail.html'
+    context_object_name = 'post'
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(
+            Post,
+            slug=self.kwargs['post'],
+            status='published',
+            publish__year=self.kwargs['year'],
+            publish__month=self.kwargs['month'],
+            publish__day=self.kwargs['day']
+        )
