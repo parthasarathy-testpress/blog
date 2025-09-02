f11a210e4b18296b4cb7beeef2db809910711e1d
parthasarathy-testpress <parthasarathy@testpress.in>
2025-09-02 12:54:23 +0530

    feat: Add share option for the post through email

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
+from django.core.mail import send_mail
+
 from django.views.generic import ListView, DetailView
 from .models import Post
+
+from .forms import EmailPostForm
+
 
 class PostListView(ListView):
     model = Post
@@ -22,3 +27,21 @@ class PostDetailView(DetailView):
             publish__month=self.kwargs['month'],
             publish__day=self.kwargs['day']
         )
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+    if request.method == 'POST':
+      form = EmailPostForm(request.POST)
+      if form.is_valid():
+        cd = form.cleaned_data
+        post_url = request.build_absolute_uri(post.get_absolute_url())
+        subject = f"{cd['name']} recommends you read {post.title}"
+        message = f"Read {post.title} at {post_url}\n\n {cd['name']}\'s comments: {cd['comments']}"
+        send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+        sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
