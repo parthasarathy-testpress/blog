cdcd0b7d7baba5b50a613f04aecc5afbc3e6b387
parthasarathy-testpress <parthasarathy@testpress.in>
2025-09-03 15:16:59 +0530

    feat: Create feeds for your blog posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import PostListView, PostDetailView, post_share
 
+from .feeds import LatestPostsFeed
+
 app_name = 'blog'
 
 urlpatterns = [
@@ -8,4 +10,5 @@ urlpatterns = [
     path('tag/<slug:tag_slug>/',PostListView.as_view(), name='post_list_by_tag'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', PostDetailView.as_view(), name='post_detail'),\
     path('<int:post_id>/share/',post_share, name='post_share'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
